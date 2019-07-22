4a21d260ad29177b13d8fd62b3fc4f20d4bbddd0
danibaxx <danibaxx@hotmail.com>
2019-07-22 17:49:32 -0600

    added app component and changed SmurfsList.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import SmurfsList from './SmurfsList';
+import { getSmurfs } from '../actions';
 import './App.css';
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +12,44 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      smurfs: '',
+      getSmurfs: '',
+      addSmurfs: '',
+      fetchingSmurfs: ''    
+    };
+  };
+  
+    componentDidMount() {
+    this.props.getSmurfs();
+    };
+
+    render() {
+    if (this.props.fetchingSmurfs) {
+     <h2>Loading... Please wait...</h2>;
+    }
+
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <SmurfsList />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    smurfs: state.smurfsReducer.smurfs,
+    getSmurfs: state.smurfsReducer.getSmurfs,
+    addSmurfs: state.smurfsReducer.addSmurfs
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { getSmurfs }
+)(App);
+
--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -1,48 +1,18 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
 
-class SmurfsList extends Component {
-  constructor() {
-    super()
-    this.state = {
-      smurfs: '',
-      getSmurfs: '',
-      addSmurfs: ''      
-    };
-  };
-    
-    componentDidMount() {
-      this.props.getSmurfs();
-    }
-
-
-  render() {
-    if (this.props.fetchingSmurfs) {
-      <h2>Loading... Please wait...</h2>;
-    }
+const SmurfsList = () => {
     return (
       <div>
         <form>
           <input type="text" name="name" placeholder="Name" value={this.state.name} />
           <input type="text" name="age" placeholder="Age" value={this.state.age} />
           <input type="text" name="height" placeholder="Height" value={this.state.height} />
+          <button onClick={() => addSmurfs(smurf)}>Add Smurf</button>
 
         </form>
       </div>
     )
-
-  }
 }
 
-const mapStateToProps = state => {
-  console.log(state);
-  return {
-    smurfs: state.smurfsReducer.smurfs,
-    getSmurfs: state.smurfsReducer.getSmurfs,
-    addSmurfs: state.smurfsReducer.addSmurfs
-  }
-}
-
-export default connect(
-  mapStateToProps
-)(SmurfsList);
+export default SmurfsList;
